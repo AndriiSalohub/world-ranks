7b95c606f73337b761ca467571758bd4a38c0da1
Andrii <andrejsalogub3@gmail.com>
2023-10-31 20:10:30 +0200

    added markup and styles for the header and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import "@/styles/globals.scss";
+import "@/styles/variables.scss";
 import { FC } from "react";
 
 const Home: FC = () => {
-    return <main>Home</main>;
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    );
 };
 
 export default Home;
---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import "@/styles/footer.scss";
+import { FC } from "react";
+
+const Footer: FC = () => {
+    return (
+        <footer className="footer">
+            Andrii Salohub | anandrejsalogub3@gmail.com
+        </footer>
+    );
+};
+
+export default Footer;

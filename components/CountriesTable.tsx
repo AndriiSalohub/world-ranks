f07bede39b5a20973d80fc21ddcdeb70ecb46784
Andrii <andrejsalogub3@gmail.com>
2023-11-04 17:29:24 +0200

    added area column to the table and also added flag near the name of the coutry

--- components/CountriesTable.tsx
+++ components/CountriesTable.tsx
@@ -1,32 +1,49 @@
 "use client";
 import { useCountries } from "@/store/store";
-import "@/styles/coutries-table.scss";
+import "@/styles/countries-table.scss";
 import { FC } from "react";
 
 const CountriesTable: FC = () => {
     const countries = useCountries((state) => state.countries);
+    console.log(countries);
 
     return (
         <section className="countries-table">
-            <button className="countries-table__buttons">
-                <p className="countries-table__buttons_name">Name</p>
-            </button>
-            <button className="countries-table__buttons">
-                <p className="countries-table__buttons_population">
-                    Population
-                </p>
-            </button>
+            <div className="countries-table__buttons">
+                <button className="countries-table__button">
+                    <p className="countries-table__button_name">Name</p>
+                </button>
+                <button className="countries-table__button">
+                    <p className="countries-table__buttons_population">
+                        Population
+                    </p>
+                </button>
+                <button className="countries-table__button">
+                    <p className="countries-table__buttons_area">Area(km^2)</p>
+                </button>
+            </div>
             <ul className="countries-table__list">
                 {countries.map((country) => (
                     <li
                         className="countries-table__list__item"
                         key={country.name.common}
                     >
-                        <p className="countries-table__list__item-name">
-                            {country.name.official}
+                        <p className="countries-table__list__item_description">
+                            <img
+                                src={country.flags.png}
+                                alt={country.flags.alt}
+                                className="countries-table__list__item_flag-img"
+                            />
+                            <p className="countries-table__list__item_name">
+                                {" "}
+                                {country.name.official}
+                            </p>
                         </p>
-                        <p className="countries-table__list__item-population">
+                        <p className="countries-table__list__item_population">
                             {country.population}
+                        </p>
+                        <p className="countries-table__list__item_area">
+                            {country.area}
                         </p>
                     </li>
                 ))}
